0a1a5b43224a78703d709526d5df07e6ba87e991
jhez <jan-olof.hellstrand@svenskaspel.se>
2018-11-29 09:34:16 +0100

    mo

--- build.sh
+++ build.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+
 gcc -Wall -std=c99  -Werror-implicit-function-declaration  rego.c -o bin/rego
+if [ $? -eq 0 ]; then
+	echo "Success!, bin/rego executable updated";
+else 
+	echo "Failure!!!!"; 
+fi
--- rego.c
+++ rego.c
@@ -17,7 +17,8 @@ typedef enum {
     RC_write_to_timer_register   = 5,    // 1 character, confirm
     RC_read_from_display         = 0x20, // response 42char text line
     RC_read_last_error_line      = 0x40, // response 42char text line
-    RC_read_prev_error_line      = 0x42  // response 42char text line
+    RC_read_prev_error_line      = 0x42, // response 42char text line
+    RC_read_firmware_version     = 0x75  // response 5 character, 16 bit number
 } RegoCommandType;
 
 typedef uint8_t RegoCommandType_u8;
@@ -27,7 +28,7 @@ typedef enum {
     REGO_TYPE_TEMP,
     REGO_TYPE_STATUS,
     REGO_TYPE_COUNTER,
-    REGO_TYPE_END
+    REGO_TYPE_ERROR_LINE
 } RegoRegType;
 
 typedef struct
@@ -40,37 +41,46 @@ typedef struct
     uint16_t    expected_reply_len;
     int16_t     reply_value;
     time_t      time;
+    int         updateable;
 } RegoCommand;
 
 static RegoCommand rego_commands[] = {
-    { "GT1 Radiator",        0x0209, REGO_TYPE_TEMP,    RC_read_from_system_register, 9, 5, 0, 0 },
-    { "GT2 Out",             0x020A, REGO_TYPE_TEMP,    RC_read_from_system_register, 9, 5, 0, 0 },
-    { "GT3 Hot water",       0x020B, REGO_TYPE_TEMP,    RC_read_from_system_register, 9, 5, 0, 0 },
-    { "GT4 Forward",         0x020C, REGO_TYPE_TEMP,    RC_read_from_system_register, 9, 5, 0, 0 },
-    { "GT5 Room",            0x020D, REGO_TYPE_TEMP,    RC_read_from_system_register, 9, 5, 0, 0 },
-    { "GT6 Compressor",      0x020E, REGO_TYPE_TEMP,    RC_read_from_system_register, 9, 5, 0, 0 },
-    { "GT8 Hot fluid out",   0x020F, REGO_TYPE_TEMP,    RC_read_from_system_register, 9, 5, 0, 0 },
-    { "GT9 Hot fluid in",    0x0210, REGO_TYPE_TEMP,    RC_read_from_system_register, 9, 5, 0, 0 },
-    { "GT10 Cold fluid in",  0x0211, REGO_TYPE_TEMP,    RC_read_from_system_register, 9, 5, 0, 0 },
-    { "GT11 Cold fluid out", 0x0212, REGO_TYPE_TEMP,    RC_read_from_system_register, 9, 5, 0, 0 },
-    { "GT3x Ext hot water",  0x0213, REGO_TYPE_TEMP,    RC_read_from_system_register, 9, 5, 0, 0 },
-    { "GT1 Target",          0x006E, REGO_TYPE_TEMP,    RC_read_from_system_register, 9, 5, 0, 0 },
-    { "GT1 On",              0x006F, REGO_TYPE_TEMP,    RC_read_from_system_register, 9, 5, 0, 0 },
-    { "GT1 Off",             0x0070, REGO_TYPE_TEMP,    RC_read_from_system_register, 9, 5, 0, 0 },
-    { "GT3 On",              0x0073, REGO_TYPE_TEMP,    RC_read_from_system_register, 9, 5, 0, 0 },
-    { "GT3 Off",             0x0074, REGO_TYPE_TEMP,    RC_read_from_system_register, 9, 5, 0, 0 },
-    { "GT4 Target",          0x006D, REGO_TYPE_TEMP,    RC_read_from_system_register, 9, 5, 0, 0 },
-    { "P3 Cold fluid",       0x01FD, REGO_TYPE_STATUS,  RC_read_from_system_register, 9, 5, 0, 0 },
-    { "Compressor",          0x01FE, REGO_TYPE_STATUS,  RC_read_from_system_register, 9, 5, 0, 0 },
-    { "Xtra 3kW",            0x01FF, REGO_TYPE_STATUS,  RC_read_from_system_register, 9, 5, 0, 0 },
-    { "Xtra 6kW",            0x0200, REGO_TYPE_STATUS,  RC_read_from_system_register, 9, 5, 0, 0 },
-    { "P1 Radiator",         0x0203, REGO_TYPE_STATUS,  RC_read_from_system_register, 9, 5, 0, 0 },
-    { "P2 Heat fluid",       0x0204, REGO_TYPE_STATUS,  RC_read_from_system_register, 9, 5, 0, 0 },
-    { "Three-way valve",     0x0205, REGO_TYPE_STATUS,  RC_read_from_system_register, 9, 5, 0, 0 },
-    { "Alarm",               0x0206, REGO_TYPE_STATUS,  RC_read_from_system_register, 9, 5, 0, 0 },
-    { "Heat-Power",          0x006C, REGO_TYPE_COUNTER, RC_read_from_system_register, 9, 5, 0, 0 },
-    { "Heatcurve",           0x0000, REGO_TYPE_COUNTER, RC_read_from_system_register, 9, 5, 0, 0 },
-    { "Heatcurve fine adj.", 0x0001, REGO_TYPE_COUNTER, RC_read_from_system_register, 9, 5, 0, 0 },
+    { "GT1 Radiator",        0x0209, REGO_TYPE_TEMP,       RC_read_from_system_register, 9, 5, 0, 0, 0 },
+    { "GT2 Out",             0x020A, REGO_TYPE_TEMP,       RC_read_from_system_register, 9, 5, 0, 0, 0 },
+    { "GT3 Hot water",       0x020B, REGO_TYPE_TEMP,       RC_read_from_system_register, 9, 5, 0, 0, 0 },
+    { "GT4 Forward",         0x020C, REGO_TYPE_TEMP,       RC_read_from_system_register, 9, 5, 0, 0, 0 },
+    { "GT5 Room",            0x020D, REGO_TYPE_TEMP,       RC_read_from_system_register, 9, 5, 0, 0, 0 },
+    { "GT6 Compressor",      0x020E, REGO_TYPE_TEMP,       RC_read_from_system_register, 9, 5, 0, 0, 0 },
+    { "GT8 Hot fluid out",   0x020F, REGO_TYPE_TEMP,       RC_read_from_system_register, 9, 5, 0, 0, 0 },
+    { "GT9 Hot fluid in",    0x0210, REGO_TYPE_TEMP,       RC_read_from_system_register, 9, 5, 0, 0, 0 },
+    { "GT10 Cold fluid in",  0x0211, REGO_TYPE_TEMP,       RC_read_from_system_register, 9, 5, 0, 0, 0 },
+    { "GT11 Cold fluid out", 0x0212, REGO_TYPE_TEMP,       RC_read_from_system_register, 9, 5, 0, 0, 0 },
+    { "GT3x Ext hot water",  0x0213, REGO_TYPE_TEMP,       RC_read_from_system_register, 9, 5, 0, 0, 0 },
+    { "GT1 Target",          0x006E, REGO_TYPE_TEMP,       RC_read_from_system_register, 9, 5, 0, 0, 0 },
+    { "GT1 On",              0x006F, REGO_TYPE_TEMP,       RC_read_from_system_register, 9, 5, 0, 0, 0 },
+    { "GT1 Off",             0x0070, REGO_TYPE_TEMP,       RC_read_from_system_register, 9, 5, 0, 0, 0 },
+    { "GT3 On",              0x0073, REGO_TYPE_TEMP,       RC_read_from_system_register, 9, 5, 0, 0, 0 },
+    { "GT3 Off",             0x0074, REGO_TYPE_TEMP,       RC_read_from_system_register, 9, 5, 0, 0, 0 },
+    { "GT4 Target",          0x006D, REGO_TYPE_TEMP,       RC_read_from_system_register, 9, 5, 0, 0, 0 },
+    { "P3 Cold fluid",       0x01FD, REGO_TYPE_STATUS,     RC_read_from_system_register, 9, 5, 0, 0, 0 },
+    { "Compressor",          0x01FE, REGO_TYPE_STATUS,     RC_read_from_system_register, 9, 5, 0, 0, 0 },
+    { "Xtra 3kW",            0x01FF, REGO_TYPE_STATUS,     RC_read_from_system_register, 9, 5, 0, 0, 0 },
+    { "Xtra 6kW",            0x0200, REGO_TYPE_STATUS,     RC_read_from_system_register, 9, 5, 0, 0, 0 },
+    { "P1 Radiator",         0x0203, REGO_TYPE_STATUS,     RC_read_from_system_register, 9, 5, 0, 0, 0 },
+    { "P2 Heat fluid",       0x0204, REGO_TYPE_STATUS,     RC_read_from_system_register, 9, 5, 0, 0, 0 },
+    { "Three-way valve",     0x0205, REGO_TYPE_STATUS,     RC_read_from_system_register, 9, 5, 0, 0, 0 },
+    { "Alarm",               0x0206, REGO_TYPE_STATUS,     RC_read_from_system_register, 9, 5, 0, 0, 0 },
+    { "Heat-Power",          0x006C, REGO_TYPE_COUNTER,    RC_read_from_system_register, 9, 5, 0, 0, 0 },
+    { "Heatcurve",           0x0000, REGO_TYPE_COUNTER,    RC_read_from_system_register, 9, 5, 0, 0, 1 },
+    { "Heatcurve fine adj.", 0x0001, REGO_TYPE_COUNTER,    RC_read_from_system_register, 9, 5, 0, 0, 1 },
+    { "Last Error",          0x0000, REGO_TYPE_ERROR_LINE, RC_read_last_error_line,      9,42, 0, 0, 0 },
+    { "Prev Last Error",     0x0000, REGO_TYPE_ERROR_LINE, RC_read_prev_error_line,      9,42, 0, 0, 0 },
+    { "LED Power",           0x0012, REGO_TYPE_STATUS,     RC_read_from_front_panel,     9, 5, 0, 0, 0 },
+    { "LED Compressor",      0x0013, REGO_TYPE_STATUS,     RC_read_from_front_panel,     9, 5, 0, 0, 0 },
+    { "LED El Tillskott",    0x0014, REGO_TYPE_STATUS,     RC_read_from_front_panel,     9, 5, 0, 0, 0 },
+    { "LED Varmvatten",      0x0015, REGO_TYPE_STATUS,     RC_read_from_front_panel,     9, 5, 0, 0, 0 },
+    { "LED Alarm",           0x0016, REGO_TYPE_STATUS,     RC_read_from_front_panel,     9, 5, 0, 0, 0 },
+    { "Firmware",            0x0000, REGO_TYPE_STATUS,     RC_read_firmware_version,     9, 5, 0, 0, 0 },
 
 };
 
@@ -148,6 +158,8 @@ static int parse_5byte_reply(const Rego5bReply* rp, int16_t* value )
     
     *value = (rp->data.value[0] << 14) | (rp->data.value[1] << 7) | rp->data.value[2];
     
+    if( rp->data.value[0] >= 2 ) (*value) -= 65536;
+    
     printf("Reply: ");
     for(int i = 0; i < sizeof( *rp ); i++ )
     {
@@ -158,7 +170,42 @@ static int parse_5byte_reply(const Rego5bReply* rp, int16_t* value )
     return 1;
 }
 
+typedef union
+{
+    uint8_t raw[42];
+    struct
+    {
+        uint8_t address; // 0x01
+        uint8_t value[3];
+        uint8_t crc;
+    } data;
+} Rego42bReply;
 
+static int parse_42byte_reply(const Rego42bReply* rp, int16_t* value )
+{
+  //  uint8_t crc = 0;
+    
+    if( sizeof( *rp ) != 5 ) exit( 1 );
+    
+    *value = 0;
+    if( rp->data.address != 0x01 ) return 0;
+    
+/*
+    for ( int i = 0; i < sizeof( rp->data.value); i++ ) crc ^= rp->data.value[i];
+    
+    if( crc != rp->data.crc ) return 0;
+    
+    *value = (rp->data.value[0] << 14) | (rp->data.value[1] << 7) | rp->data.value[2];
+*/
+    printf("Reply: ");
+    for(int i = 0; i < sizeof( *rp ); i++ )
+    {
+        printf("%02x", rp->raw[i] );
+    }
+    puts("");
+    
+    return 1;
+}
 
 static int open_serial(const char* port_p)
 {
@@ -226,7 +273,7 @@ int main( int argc, char* argv[] )
     
   //  if( fd >= 0 )
     {
-        RegoCommand* rc_p = &rego_commands[1]; // GT2.
+        RegoCommand* rc_p = &rego_commands[sizeof( rego_commands)/sizeof(*rc_p)-1]; // GT2.
         int bytes_written = send_request( fd, rc_p );
         
         if( bytes_written == rc_p->request_len )
@@ -246,6 +293,21 @@ int main( int argc, char* argv[] )
                     else
                         printf("Parse of reply to %s failed\n", rc_p->name );
                 }
+                else if( bytes_read == 42 )
+                {
+                    if( parse_42byte_reply( (const Rego42bReply*) buff, &rc_p->reply_value ) )
+                    {
+                        printf("Current value of %s is %d\n", rc_p->name, rc_p->reply_value );
+                        exit_status = EXIT_SUCCESS;
+                    }
+                    else
+                        printf("Parse of reply to %s failed\n", rc_p->name );
+
+                }
+            }
+            else
+            {
+                printf("Unexpectedly only got %d bytes\n", bytes_read );
             }
         }
         close( fd );
